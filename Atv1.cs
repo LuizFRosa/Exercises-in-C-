4434e0ddce9203cd8de45c318a2a5359b6f8db93
Luiz Filipe Da Rosa <169152129+LuizFRosa@users.noreply.github.com>
2025-01-28 08:52:29 -0300

    Create Atv1.cs

---
+++ Atv1.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Exercicios_em_C_
+{
+	class Program
+	{
+		public static void Main()
+		{
+			Console.Write("Digite um número inteiro: ");
+			int numero = int.Parse(Console.ReadLine());
+			Console.WriteLine("O número digitado foi o: " + numero);
+		}
+	}
+}
